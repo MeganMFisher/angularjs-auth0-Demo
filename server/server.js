602af16587296b207128a21e8ded1264b1cdeef5
MeganMFisher <meganmichelle318@gmail.com>
2017-06-20 20:38:51 -0600

    adding auth0

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require('express'),
+      session = require('express-session'),
       bodyParser = require('body-parser'),
       massive = require('massive'),
       config = require('./config.js')
+      passport = require('passport'),
+      Auth0Strategy = require('passport-auth0'),
+      cors = require('cors'),
+      request = require('request'),
+      controller = require('./controller.js');
 
 massive(config.database).then(db => {
     app.set('db', db)
@@ -13,8 +19,50 @@ var port = 3000;
 
 var app = express()
 app.use(bodyParser.json())
+app.use(session({
+    secret: config.sessionSecret,
+    saveUninitialized: true,
+    resave: false
+}))
 
-const controller = require('./controller.js')
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new Auth0Strategy({
+  domain: config.authDomain,
+  clientID: config.clientId,
+  clientSecret: config.clientSecret,
+  callbackURL: 'http://localhost:3000/auth/callback'
+}, function(accessToken, refreshToken, extraParams, profile, done) {
+  return done(null, profile);
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+app.get('/auth', passport.authenticate('auth0'));
+
+app.get('/auth/callback',
+  passport.authenticate('auth0', {successRedirect: '/'}), function(req, res) {
+    res.status(200).send(req.user);
+})
+
+app.get('/auth/me', function(req, res) {
+  if (!req.user) return res.sendStatus(404);
+  res.status(200).send(req.user);
+})
+
+
+
+
+
+
 
 
 app.listen(port, function() {

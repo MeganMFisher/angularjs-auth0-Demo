f30f7f2f3884a7f09e51c56c59b895f4a765a720
MeganMFisher <meganmichelle318@gmail.com>
2017-06-20 21:12:23 -0600

    login stuff

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -1,5 +1,25 @@
 angular.module('app').controller('mainCtrl', function($scope, mainSrv){
 
+  function getUser() {
+    mainSrv.getUser().then(function(user) {
+      if (user) $scope.user = user.username;
+      else   $scope.user = 'NOT LOGGED IN';
+    })
+  }
 
+  getUser();
+
+  $scope.loginLocal = function(username, password) {
+    console.log('Logging in with', username, password);
+    mainSrv.loginLocal({
+      username: username,
+      password: password
+    })
+    .then(function(res) {
+      getUser();
+    })
+  }
+
+  $scope.logout = mainSrv.logout;
 
 })

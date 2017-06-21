d4a30be3b07517db15f4768b38b8f83e6871f674
MeganMFisher <meganmichelle318@gmail.com>
2017-06-20 19:49:32 -0600

    ui-router setup

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
-angular.module('app', [])
+angular.module('app', ['ui.router'])
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/');
+
+
+        $stateProvider
+            .state('login', {
+                url: '/',
+                templateUrl: './views/login.html',
+            })
+            .state('home', {
+                url: '/home',
+                templateUrl: './views/home.html',
+            })
+
+    })

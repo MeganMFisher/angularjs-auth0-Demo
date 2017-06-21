684140d3bd7ca6be15ecd8ff90d72548df52a38b
MeganMFisher <meganmichelle318@gmail.com>
2017-06-20 21:08:07 -0600

    config switcharoo

--- server/server.js
+++ server/server.js
@@ -15,35 +15,62 @@ massive(config.database).then(db => {
     console.log(err)
 })
 
-var port = 3000;
+var port = 3001;
 
-var app = express()
-app.use(bodyParser.json())
+const app = express();
+
+app.use(bodyParser.json());
 app.use(session({
-    secret: config.sessionSecret,
-    saveUninitialized: true,
-    resave: false
+  resave: true, //Without this you get a constant warning about default values
+  saveUninitialized: true, //Without this you get a constant warning about default values
+  secret: config.secret
 }))
-
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(express.static('./public'));
+
 passport.use(new Auth0Strategy({
-  domain: config.authDomain,
-  clientID: config.clientId,
-  clientSecret: config.clientSecret,
-  callbackURL: 'http://localhost:3000/auth/callback'
-}, function(accessToken, refreshToken, extraParams, profile, done) {
-  return done(null, profile);
-}));
+   domain:       config.auth0.domain,
+   clientID:     config.auth0.clientID,
+   clientSecret: config.auth0.clientSecret,
+   callbackURL:  '/auth/callback'
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    //Find user in database
+    db.getUserByAuthId([profile.id], function(err, user) {
+      user = user[0];
+      if (!user) { //if there isn't one, we'll create one!
+        console.log('CREATING USER');
+        db.createUserByAuth([profile.displayName, profile.id], function(err, user) {
+          console.log('USER CREATED', userA);
+          return done(err, user[0]); // GOES TO SERIALIZE USER
+        })
+      } else { //when we find the user, return it
+        console.log('FOUND USER', user);
+        return done(err, user);
+      }
+    })
+  }
+));
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
+//THIS IS INVOKED ONE TIME TO SET THINGS UP
+passport.serializeUser(function(userA, done) {
+  console.log('serializing', userA);
+  var userB = userA;
+  //Things you might do here :
+   //Serialize just the id, get other information to add to session, 
+  done(null, userB); //PUTS 'USER' ON THE SESSION
 });
 
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
+//USER COMES FROM SESSION - THIS IS INVOKED FOR EVERY ENDPOINT
+passport.deserializeUser(function(userB, done) {
+  var userC = userC;
+  //Things you might do here :
+    // Query the database with the user id, get other information to put on req.user
+  done(null, userC); //PUTS 'USER' ON REQ.USER
 });
+
 
 
 app.get('/auth', passport.authenticate('auth0'));
@@ -55,9 +82,14 @@ app.get('/auth/callback',
 
 app.get('/auth/me', function(req, res) {
   if (!req.user) return res.sendStatus(404);
+  //THIS IS WHATEVER VALUE WE GOT FROM userC variable above.
   res.status(200).send(req.user);
 })
 
+app.get('/auth/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+})
 
 
 

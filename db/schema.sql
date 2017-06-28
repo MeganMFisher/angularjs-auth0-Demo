e412ba8724e174f1882d8110e4f92e35446c2973
MeganMFisher <meganmichelle318@gmail.com>
2017-06-28 09:15:38 -0600

    login working!

--- db/schema.sql
+++ db/schema.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS users
 CREATE TABLE users
 (
   id serial primary key,
-    user_id int
+  username varchar, 
+  authid varchar
 );
--- server/server.js
+++ server/server.js
@@ -9,65 +9,69 @@ const express = require('express'),
       request = require('request'),
       controller = require('./controller.js');
 
-massive(config.database).then(db => {
-    app.set('db', db)
-}).catch((err) => {
-    console.log(err)
-})
-
-var port = 3000;
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(session({
   resave: true, //Without this you get a constant warning about default values
   saveUninitialized: true, //Without this you get a constant warning about default values
-  secret: config.secret
+  secret: 'keyboardcat'
 }))
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(express.static('./public'));
 
+massive(config.database).then(db => {
+  app.set('db', db)
+}).catch((err) => {
+  console.log(err)
+})
+
+/////////////
+// DATABASE //
+/////////////
+
 passport.use(new Auth0Strategy({
-   domain:       config.auth0.domain,
-   clientID:     config.auth0.clientID,
-   clientSecret: config.auth0.clientSecret,
-   callbackURL:  '/auth/callback'
+    domain: config.auth0.domain,
+    clientID: config.auth0.clientID,
+    clientSecret: config.auth0.clientSecret,
+    callbackURL: '/auth/callback'
   },
-  function(accessToken, refreshToken, extraParams, profile, done) {
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    console.log(profile)
+    var db = app.get('db')
     //Find user in database
-    db.getUserByAuthId([profile.id], function(err, user) {
+    db.getUserByAuthId(profile.id).then(function (user) {
       user = user[0];
       if (!user) { //if there isn't one, we'll create one!
         console.log('CREATING USER');
-        db.createUserByAuth([profile.displayName, profile.id], function(err, user) {
-          console.log('USER CREATED', userA);
-          return done(err, user[0]); // GOES TO SERIALIZE USER
+        db.createUserByAuth([profile.displayName, profile.id]).then(function (user) {
+          console.log('USER CREATED', user);
+          return done(null, user[0]); // GOES TO SERIALIZE USER
         })
       } else { //when we find the user, return it
         console.log('FOUND USER', user);
-        return done(err, user);
+        return done(null, user);
       }
     })
   }
 ));
 
 //THIS IS INVOKED ONE TIME TO SET THINGS UP
-passport.serializeUser(function(userA, done) {
+passport.serializeUser(function (userA, done) {
   console.log('serializing', userA);
   var userB = userA;
   //Things you might do here :
-   //Serialize just the id, get other information to add to session, 
+  //Serialize just the id, get other information to add to session, 
   done(null, userB); //PUTS 'USER' ON THE SESSION
 });
 
 //USER COMES FROM SESSION - THIS IS INVOKED FOR EVERY ENDPOINT
-passport.deserializeUser(function(userB, done) {
-  var userC = userC;
+passport.deserializeUser(function (userB, done) {
+  var userC = userB;
   //Things you might do here :
-    // Query the database with the user id, get other information to put on req.user
+  // Query the database with the user id, get other information to put on req.user
   done(null, userC); //PUTS 'USER' ON REQ.USER
 });
 
@@ -75,28 +79,34 @@ passport.deserializeUser(function(userB, done) {
 
 app.get('/auth', passport.authenticate('auth0'));
 
-app.get('/auth/callback',
-  passport.authenticate('auth0', {successRedirect: '/'}), function(req, res) {
-    res.status(200).send(req.user);
-})
 
-app.get('/auth/me', function(req, res) {
+//**************************//
+//To force specific provider://
+//**************************//
+// app.get('/login/google',
+//   passport.authenticate('auth0', {connection: 'google-oauth2'}), function (req, res) {
+//   res.redirect("/");
+// });
+
+app.get('/auth/callback',
+  passport.authenticate('auth0', {
+    successRedirect: '/'
+  }),
+  function (req, res) {
+    res.status(200).send(req.user);
+  })
+
+app.get('/auth/me', function (req, res) {
   if (!req.user) return res.sendStatus(404);
   //THIS IS WHATEVER VALUE WE GOT FROM userC variable above.
   res.status(200).send(req.user);
 })
 
-app.get('/auth/logout', function(req, res) {
+app.get('/auth/logout', function (req, res) {
   req.logout();
   res.redirect('/');
 })
 
-
-
-
-
-
-
-app.listen(port, function() {
-  console.log("Started server on port", port);
-});
+app.listen(3005, function () {
+  console.log('Connected on 3005')
+})
